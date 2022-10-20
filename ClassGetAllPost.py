4426658df6e4eb63340ce057b4676c29dd6c47c2
Theeramet Metha <theeramet.m@apollo21.asia>
2022-10-20 23:37:13 +0700

    Test online DB

--- ClassGetAllPost.py
+++ ClassGetAllPost.py
@@ -7,4 +7,26 @@ import pymysql
 
 class GetAllPost(Resource):
     def get(self):
-        return "Test Hello"
+        connection = pymysql.connect(host='sql6.freesqldatabase.com', user='sql6528008', password='ygfdb6iNRv',db='sql6528008')
+        mycursor = connection.cursor()
+        mycursor.execute("SELECT * FROM TestTable WHERE id > %s",'0')
+        user = mycursor.fetchall()
+        connection.commit()
+        connection.close()  
+        
+        #convert to json format
+        rowarray_list = []
+        for row in user:
+            t = (row[0],row[1],row[2])
+            rowarray_list.append(t)
+        json.dumps(rowarray_list)
+
+        object_list = []
+        for row in user:
+            d = collections.OrderedDict()
+            d['Id'] = row[0]
+            d['Name'] = row[1]
+            d['Age'] = row[2]
+            object_list.append(d)
+        l = json.dumps(object_list)
+        return json.loads(l)
